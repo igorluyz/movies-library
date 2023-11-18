c692ff21cbfa5d7cbc760810b11f122ea87c455a
Igor Luiz <igorprofessionalcontact@gmail.com>
2023-11-18 20:36:45 -0300

    configurações no path base

--- src/main.jsx
+++ src/main.jsx
@@ -16,7 +16,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route element={<App />}>
+        <Route path="/movies-library" element={<App />}>
           <Route path="/" element={<Home />} />
           <Route path="movie/:id" element={<Movie />} />
           <Route path="search" element={<Search />} />
